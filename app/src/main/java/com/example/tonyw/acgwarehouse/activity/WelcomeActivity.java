0d01713896f4aec501cb37e1e691298d822a7790
Tonywu10 <tonywu1061@gmail.com>
2017-03-31 09:17:16 +0800

    修改了启动动画

--- app/src/main/java/com/example/tonyw/acgwarehouse/activity/WelcomeActivity.java
+++ app/src/main/java/com/example/tonyw/acgwarehouse/activity/WelcomeActivity.java
@@ -3,6 +3,8 @@ package com.example.tonyw.acgwarehouse.activity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.Window;
+import android.view.WindowManager;
 import android.view.animation.AlphaAnimation;
 import android.view.animation.Animation;
 import android.widget.ImageView;
@@ -14,6 +16,8 @@ public class WelcomeActivity extends AppCompatActivity{
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,WindowManager.LayoutParams.FLAG_FULLSCREEN);
         setContentView(R.layout.activity_welcome);
         welcomeView= (ImageView) findViewById(R.id.welcome);
         AlphaAnimation animate = new AlphaAnimation(1.0f,1.0f);
